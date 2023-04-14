a75244d8ef35e5acd92c6eace4303e51bbe8a1c0
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2023-04-14 19:28:34 -0300

    .
    .

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
+console.log(process.env.GOOGLE_CLIENT_ID)
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
-
+import Login from './Login'
 async function Nav() {
   return (
     <nav>
       <Link href={'/'}>
         <h1>Entrar</h1>
       </Link>
+      <ul>
+        <Login />
+      </ul>
     </nav>
   )
 }
