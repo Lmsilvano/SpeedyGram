aee2e29bbbfa9a0f48e419b2d2eb854cfbd5b606
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2023-04-14 20:05:49 -0300

    Update Login.tsx

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -3,15 +3,15 @@ import { signIn } from 'next-auth/react'
 
 function Login() {
 
-  
+
   return (
     <li className="list-none">
       <button className="text-sm" onClick={() => signIn()}>
         Logar
       </button>
     </li>
-  ) 
-} 
+  )
+}
 
 
 export default Login
